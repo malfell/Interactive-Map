6f95dca665aade695ea8591600c8a2c8819d5984
malfell <malfellows13@gmail.com>
2022-05-05 19:18:01 -0500

    added marker layer group

--- public/app.js
+++ public/app.js
@@ -39,5 +39,18 @@ var polygon = L.polygon(latlngs, {
 
 // metro station markers
 
+// Metro station markers:
+//list of markers with popups
+const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+
+//grouping stations as a layergroup and adding to the map
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap);
+
+
 
 
