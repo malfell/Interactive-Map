27dc85386a405e9ba613603bcbc0a858e32d7ee3
malfell <malfellows13@gmail.com>
2022-05-05 19:12:01 -0500

    drew polygon

--- public/app.js
+++ public/app.js
@@ -19,7 +19,19 @@ const marker = L.marker([48.87007, 2.346453])
 marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
-// draw the 2nd arrondissement
+// Draw the 2nd Arrondissement     
+//latitudes and longitudes                                     
+var latlngs = [[48.863368120198004, 2.3509079846928516], [48.86933262048345, 2.3542531602919805], 
+[48.87199261164275, 2.3400569901592183], [48.86993336274516, 2.3280142476578813], 
+[48.86834104280146, 2.330308418109664]
+]; 
+
+//creating the polygon
+var polygon = L.polygon(latlngs, {
+    color: 'blue', 
+    fillOpacity: 0.0,
+
+}).addTo(myMap); //adding it to the map
 
 
 // create red pin marker
