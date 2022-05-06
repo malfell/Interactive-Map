ec04de8ff019da6a09d97521e4c5f9cb99fe5b9e
malfell <malfellows13@gmail.com>
2022-05-05 19:02:30 -0500

    created map with main marker

--- public/app.js
+++ public/app.js
@@ -1,10 +1,22 @@
 // create map
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+})
 
 
-// add openstreetmap tiles
+// Add OpenStreetMap tiles:
+//Need a map layer!
+//That {s}, {z}, {x}, and {y} are very important here
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
-// create and main add geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // draw the 2nd arrondissement
